09bd4c21af1899bba01c467267b164ee50ad8dc8
ARNOLD ACHIKI <achikiarnold@gmail.com>
2024-12-15 13:33:28 +0300

    Your descriptive commit message

--- DONA/urls.py
+++ DONA/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.contrib.auth import views as auth_views
+from users import views
+
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('donate/', views.donate, name='donate'),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
 
 # Serve static files during development (only if DEBUG is True)
